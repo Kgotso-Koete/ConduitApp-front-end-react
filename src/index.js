a1267cd63e407fdefde512f1b9ae8c4295578585
“Kgotso-Koete” <kgkoete@gmail.com>
2019-11-10 17:42:34 +0200

    Add Login component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import React from "react";
 import store from "./store";
 import Home from "./components/Home";
-// import Login from "./components/Login";
+import Login from "./components/Login";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 ReactDOM.render(
@@ -12,8 +12,7 @@ ReactDOM.render(
     <Router history={hashHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Home} />
-        //
-        <Route path="login" /> -->
+        <Route path="login" component={Login} />
       </Route>
     </Router>
   </Provider>,
