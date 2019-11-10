15409d0fc3cd3beff827445afc95a2b22cbd0d23
“Kgotso-Koete” <kgkoete@gmail.com>
2019-11-10 19:18:12 +0200

    Wire up the Login form

--- src/agent.js
+++ src/agent.js
@@ -8,13 +8,21 @@ const API_ROOT = "https://conduit.productionready.io/api";
 const responseBody = res => res.body;
 
 const requests = {
-  get: url => superagent.get(`${API_ROOT}${url}`).then(responseBody)
+  get: url => superagent.get(`${API_ROOT}${url}`).then(responseBody),
+  post: (url, body) =>
+    superagent.post(`${API_ROOT}${url}`, body).then(responseBody)
 };
 
 const Articles = {
   all: page => requests.get(`/articles?limit=10`)
 };
 
+const Auth = {
+  login: (email, password) =>
+    requests.post("/users/login", { user: { email, password } })
+};
+
 export default {
-  Articles
+  Articles,
+  Auth
 };
