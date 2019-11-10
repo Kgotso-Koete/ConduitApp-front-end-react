e7049591d1a485564eef086fb502ab86cc969843
“Kgotso-Koete” <kgkoete@gmail.com>
2019-11-10 17:34:37 +0200

    Add routing structure

--- src/components/App.js
+++ src/components/App.js
@@ -12,11 +12,15 @@ class App extends React.Component {
     return (
       <div>
         <Header appName={this.props.appName} />
-        <Home />
+        {this.props.children}
       </div>
     );
   }
 }
+
+App.contextTypes = {
+  router: React.PropTypes.object.isRequired
+};
 
 export default connect(
   mapStateToProps,
