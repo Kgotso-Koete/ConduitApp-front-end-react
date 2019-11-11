447e5cd8a7203bc9ef66ac4f28996e8f2af7d53d
“Kgotso-Koete” <kgkoete@gmail.com>
2019-11-11 12:13:55 +0200

    Add Settings feature

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App from "./components/App";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Settings from "./components/Settings";
 import store from "./store";
 
 ReactDOM.render(
@@ -16,6 +17,7 @@ ReactDOM.render(
         <IndexRoute component={Home} />
         <Route path="login" component={Login} />
         <Route path="register" component={Register} />
+        <Route path="settings" component={Settings} />
       </Route>
     </Router>
   </Provider>,
--- src/store.js
+++ src/store.js
@@ -1,16 +1,18 @@
 import { applyMiddleware, createStore, combineReducers } from "redux";
-import { promiseMiddleware } from "./middleware";
+import { promiseMiddleware, localStorageMiddleware } from "./middleware";
 import auth from "./reducers/auth";
 import common from "./reducers/common";
 import home from "./reducers/home";
+import settings from "./reducers/settings";
 
 const reducer = combineReducers({
   auth,
   common,
-  home
+  home,
+  settings
 });
 
-const middleware = applyMiddleware(promiseMiddleware);
+const middleware = applyMiddleware(promiseMiddleware, localStorageMiddleware);
 
 const store = createStore(reducer, middleware);
 
